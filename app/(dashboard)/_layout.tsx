abdc84f7fdb303891ed4ed81c0ec6db5898c054d
kavindisathsarani <dksmanimendra0@gmail.com>
2025-08-15 11:42:17 +0530

    Implement Dashboard layout with tab navigation; add TaskLayout component

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,9 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { Tabs } from "expo-router";
+import React from "react";
+import { SafeAreaView } from "react-native";
 
-const DashboardLayout = () => {
-  return <Tabs />
-}
+const DashBoardLayout = () => {
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#007AFF",
+          tabBarInactiveTintColor: "#8E8E93",
+          tabBarStyle: {
+            backgroundColor: "#FFFFFF",
+            borderTopWidth: 0,
+            elevation: 0,
+          }
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            tabBarIcon: (data) => (
+              <AntDesign name="home" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: (data) => (
+              <AntDesign name="profile" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="setting"
+          options={{
+            title: "Settings",
+            tabBarIcon: (data) => (
+              <AntDesign name="setting" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="tasks"
+          options={{
+            title: "Tasks",
+            tabBarIcon: (data) => (
+              <FontAwesome5 name="tasks" size={24} color="black" />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
+  );
+};
 
-export default DashboardLayout
+export default DashBoardLayout;
