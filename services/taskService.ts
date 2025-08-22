9492523f84bd28187bb141de989a9c4cf48f4b91
kavindisathsarani <dksmanimendra0@gmail.com>
2025-08-22 11:21:07 +0530

    Add Firestore integration for task management; define Task interface and implement task creation functionality

--- services/taskService.ts
+++ services/taskService.ts
@@ -1,4 +1,16 @@
+import { Task } from "@/types/task";
 import api from "./config/api"
+import { addDoc,collection } from "firebase/firestore";
+import { db } from "@/firebase";
+
+//for refer to collection
+export const taskColRef = collection(db,"tasks")
+
+//firebase firestore
+export const createTask = async (task:Task) => {
+    const docRef = await addDoc(taskColRef,task)
+    return docRef.id
+}
 
 export const getAllTasks = async () => {
     const res = await api.get("/task");
@@ -9,3 +21,5 @@ export const addTask = async (task:any) => {
     const res = await api.post("/task", task);
     return res.data;
 }
+
+
